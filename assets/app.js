10ad08ec2572b64ef91ce4caef6379ec6d958481
Ryan Hayden <ryanhayden@ryans-mbp.attlocal.net>
2017-10-17 21:36:23 -0500

    added html, css, js

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+$(document).ready (function() {
+
+$('#animalButton').on('click', function() {
+
+	var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats"
+	$.ajax({url: queryURL, method: 'GET'})
+
+            .done(function(response) {
+            var imageUrl = response.data.image_original_url;
+            var catImage = $("<img>");
+            catImage.attr('src', imageUrl);
+            catImage.attr('alt', 'cat image');
+            $('#images').prepend(catImage);
+});
+
+    $('#addAnimal').on('click', function() {
+
+        var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + "input";
+
+        $.ajax({url: queryURL, method: 'GET'})
+
+            .done(function(response) {
+
+
+                var imageUrl = response.data.image_original_url;
+
+                var catImage = $("<img>");
+
+                catImage.attr('src', imageUrl);
+                catImage.attr('alt', 'cat image');
+
+                $('#images').prepend(catImage);
+            });
+})
